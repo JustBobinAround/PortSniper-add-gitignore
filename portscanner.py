5006ca8904b7c8b7f1ca43f0c5b5854976c33ab9
ForbiddenShadow <forbiddenshadow@protonmail.com>
2023-08-02 16:47:43 +0300

    Began working on getting arguments

--- portscanner.py
+++ portscanner.py
@@ -1,7 +1,19 @@
 import scapy
+import argparse
 
-src = "192.168.1.10"
-dst = "72.14.207.99"
-port = "80"
+def getArgs():
+    parser = argparse.ArgumentParser(
+        prog="Port Sniper",
+        description="A simple and efficient port-scanner"
+        )
 
-sr1(IP(dst=dst)/TCP(dport=port,flags="S"))
+    parser.add_argument('-i','--ip', type=str, help='The machine IP to scan', dest='ip', required=True)
+    parser.add_argument('-p','--port', type=str, help='Port range to scan seperated by a dash, ex: 80 or 1-100', dest='port', required=True)
+
+    args = parser.parse_args()
+
+    return args
+
+
+args = getArgs()
+print(args.ip, args.port)
