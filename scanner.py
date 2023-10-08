bf3f58a69b9ad8dd80abe502c2439f9fcaa8c733
Ameer Moustafa <ameer.moustafa1@gmail.com>
2023-10-09 02:21:37 +0300

    added syn scan to class

--- scanner.py
+++ scanner.py
@@ -16,3 +16,9 @@ class Scanner:
         self.ping_packet = IP(dst=self.ip)/ICMP()
         self.ping_response = sr1(self.ping_packet, timeout=1, verbose=0)
         return self.ping_response
+    
+    def syn(self):
+    self.syn_packet = IP(dst=self.ip)/TCP(dport=self.port, flags='S')
+    self.rest_packet = IP(dst=self.ip)/TCP(dport=self.port, flags='R')
+    self.response = sr1(self.syn_packet, verbose=0, timeout=1)
+    return self.response
